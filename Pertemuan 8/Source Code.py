92dfc91207bffd6bcfcbdbf1be9ea72a320fbbb5
Darren <darrenmalvino75@gmail.com>
2025-04-27 16:51:12 +0700

    pertemuan 8

---
+++ Pertemuan 8/Source Code.py
@@ -0,0 +1,37 @@
+# contoh = "Hello World!"
+# contoh2 = "Ini adalah String"
+# print(contoh [2])
+# print(contoh2[3])
+
+# hewan = ['kucing', 'babi', 'ayam']
+# print('babi' in hewan) #True
+# print('burung' in hewan) #False
+
+# kata = "program"
+# if 'p' in kata:
+#     print("'p' ditemukan dalam kata.") # akan ke print 
+# else:
+#     print("'p' tidak ditemukan.") # Tidak akan ke print
+
+# print(len('HAII')) # 4
+# print(len('Halo Hai')) # 8
+
+# kalimat = ("Aku","kamu","dia")
+# print(len(kalimat)) #3
+data = ("aku adalah guru")
+print (data [len(data)-2]) #r huruf dari terakhir ke dua
+
+kata = "Ada"
+for i in kata:
+    print(i) 
+
+teks = "Murphy"
+hasil = ""
+
+for huruf in teks:
+    hasil = huruf + hasil 
+
+print("Kebalikannya:", hasil)
+
+teks = ("Lukisan")
+print(teks[0:4]) #outputnya Luki
