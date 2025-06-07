770bba4e84125b2011b66ba6917d529d00b1638e
Darren <darrenmalvino75@gmail.com>
2025-06-07 18:41:24 +0700

    .

--- Pertemuan 14 Rekursif/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def kombinasi(n,r):
-    if n == r or r == 0:
-        return 1 
-    else:
-        return kombinasi(n-1,r) + kombinasi (n-1,r-1)
-print(kombinasi(5,2))
-print(kombinasi(5,0))
-print(kombinasi(1,1))
